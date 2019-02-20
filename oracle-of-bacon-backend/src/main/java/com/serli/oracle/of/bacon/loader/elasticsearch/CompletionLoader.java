76f0fc62626990ccf06a29e74509a67d4e16d026
44chloeguilbaud@gmail.com <44chloeguilbaud@gmail.com>
2019-02-20 14:45:13 +0100

    feat: elasticsearch completion suggester

--- oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
+++ oracle-of-bacon-backend/src/main/java/com/serli/oracle/of/bacon/loader/elasticsearch/CompletionLoader.java
@@ -1,17 +1,29 @@
 package com.serli.oracle.of.bacon.loader.elasticsearch;
 
 import com.serli.oracle.of.bacon.repository.ElasticSearchRepository;
+import org.elasticsearch.action.ActionListener;
+import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
+import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
+import org.elasticsearch.action.admin.indices.mapping.put.PutMappingRequest;
+import org.elasticsearch.action.bulk.BulkProcessor;
 import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.unit.ByteSizeUnit;
+import org.elasticsearch.common.unit.ByteSizeValue;
+import org.elasticsearch.common.unit.TimeValue;
 import org.elasticsearch.common.xcontent.XContentType;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.UUID;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.BiConsumer;
 
 public class CompletionLoader {
     private static AtomicInteger count = new AtomicInteger(0);
@@ -26,13 +38,80 @@ public class CompletionLoader {
             System.exit(-1);
         }
 
+        PutMappingRequest request = new PutMappingRequest("actors");
+        request.type("actor");
+        String bodyRequest = "{" +
+                                "\"properties\": " +
+                                    "{ " +
+                                        "\"suggest\": " +
+                                            "{ " +
+                                                "\"type\": \"completion\"" +
+                                            "}," +
+                                        "\"name\" : " +
+                                            "{" +
+                                                "\"type\": \"text\"" +
+                                            "}" +
+                                    "}" +
+                            "}";
+
+        request.source(bodyRequest, XContentType.JSON);
+
+        BulkProcessor.Listener listener = new BulkProcessor.Listener() {
+
+            @Override
+            public void beforeBulk(long executionId, BulkRequest request) {
+                // Rien à faire
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, BulkResponse response) {
+                count.incrementAndGet();
+                System.out.println("Inserted total of " + count.get() + " bulk actors");
+                //System.out.println("Inserted total of about " + count.get()*5000 + " actors");
+            }
+
+            @Override
+            public void afterBulk(long executionId, BulkRequest request, Throwable failure) {
+                System.err.println("Fail");
+                failure.printStackTrace();
+            }
+
+        };
+
+        BiConsumer<BulkRequest, ActionListener<BulkResponse>> bulkConsumer =
+                (bulkRequest, bulkListener) ->
+                        client.bulkAsync(bulkRequest, bulkListener);
+
+        BulkProcessor.Builder builder = BulkProcessor.builder(bulkConsumer, listener);
+        builder.setFlushInterval(TimeValue.timeValueSeconds(10));
+        builder.setBulkActions(50000);
+        builder.setBulkSize(new ByteSizeValue(5, ByteSizeUnit.MB));
+        builder.setConcurrentRequests(0);
+
+        BulkProcessor bulkProcessor = builder.build();
+
         String inputFilePath = args[0];
         try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(inputFilePath))) {
             bufferedReader
                     .lines()
                     .forEach(line -> {
                         // TODO ElasticSearch insert
-                        try {
+                        if (count.get() < 1) {
+                            count.incrementAndGet();
+                            return;
+                        } else {
+                            IndexRequest indRequest = new IndexRequest("actors", "actor");
+                            Map<String, Object> map = new HashMap<>();
+                            String name = line.substring(1, line.length() - 1);
+                            map.put("name", name);
+                            String[] splitName = name.split("\\s+");
+                            map.put("suggest", splitName);
+                            indRequest.source(map);
+                            bulkProcessor.add(indRequest);
+                        }
+
+                        // Insertions sans BulkProcessor
+                        /*try {
                             BulkRequest request = new BulkRequest();
                             String nom = "";
                             String prenom = "";
@@ -51,11 +130,9 @@ public class CompletionLoader {
                         } catch (IOException e) {
                             e.printStackTrace();
                         }
-                        System.out.println(line);
+                        System.out.println(line);*/
                     });
         }
-
-        System.out.println("Inserted total of " + count.get() + " actors");
 
         client.close();
     }
